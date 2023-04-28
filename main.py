cd8d9e18256d4781090076ab32b3146c485cff19
Sujong Ham <rnfhrn960@gmail.com>
2023-04-28 13:37:00 +0900

    update

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get('/hello')
 def Hello():
-    return {"message": '안녕하세요'}
+    return {"message": '서버 메시지'}
