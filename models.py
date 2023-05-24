778860c017197c162bb9166efc92ab5dc1d65672
Sujong Ham <rnfhrn960@gmail.com>
2023-05-24 20:36:08 +0900

    update alembic

--- models.py
+++ models.py
@@ -1,51 +1,32 @@
-from sqlalchemy import Column, String, Integer, Text, DateTime, ForeignKey
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String, Integer, Text, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from database import engine
 Base = declarative_base()
 
 
-# 사용자 모델
 class Note(Base) :
     __tablename__ = "note"
 
     id = Column(Integer, nullable=False, primary_key=True)
     title = Column(String(45), nullable=False)
     body = Column(Text, nullable=False)
+    tags = Column(String(100), nullable=True)
     writer_id = Column(Integer, nullable=False)
-    # writer_id = Column(Integer, ForeignKey("writer.id"))
-    # writer = relationship("User", backref="notes")
+    created_date = Column(DateTime, nullable=False)
     last_updated_date = Column(DateTime, nullable=False)
 
 
-# 사용자 모델
 class User(Base) :
     __tablename__ = "user"
 
     id = Column(Integer, nullable=False, primary_key=True, autoincrement=True)
+    uid = Column(String(12), nullable=False)
     email = Column(String(100), nullable=False)
-    password = Column(String(100),nullable=False)
+    social = Column(String(10), nullable=True)
     name = Column(String(100),nullable=True)
-    social = Column(String(100), nullable=True)
-    # token = Column(String(1000), nullable=True)
-    # goals = Column(String(100),nullable=True)
-    # groups = Column(String(100),nullable=True)
-    profile_image = Column(String(100), default = "assets/images/default_profile.png")
-    # notes = Column(String(100),nullable=True)
-    # chats = Column(String(100),nullable=True)
+    info = Column(String(100), nullable=True)
+    notes = Column(Text, nullable=True)
+    schedules = Column(Text, nullable=True)
 
-
-
-    # def __init__(self, id, password, name,
-    #              # goals, groups, profile_image, notes, chats
-    #              ):
-    #     self.id = id
-    #     self.password = password
-    #     self.name = name
-        # self.goals = goals
-        # self.groups = groups
-        # self.profile_image = profile_image
-        # self.notes = notes
-        # self.chats = chats
 
 Base.metadata.create_all(bind=engine)
